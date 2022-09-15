ca27ba2c0b9a882c930691c409881222f308d987
xiaolin.zhang <xiaolin.zhang@advantech.com.cn>
2022-09-15 03:32:06 -0700

    modify HJ15

--- cmd/binary/HJ15.go
+++ cmd/binary/HJ15.go
@@ -6,15 +6,15 @@ import (
 
 func main() {
 	input := 0
-	for {
-		fmt.Scan(&input)
-		num := 0
-		for i := 0; i < 32; i++ {
-			if input&1 == 1 {
-				num++
-			}
-			num = num >> 1
+
+	fmt.Scan(&input)
+	num := 0
+	for i := 0; i < 32; i++ {
+		if input&1 == 1 {
+			num++
 		}
-		fmt.Print(num)
+		input = input >> 1
 	}
+	fmt.Print(num)
+
 }
